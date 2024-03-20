e9e3c2e9958f12b4068d4df88987cbce1339968c
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2024-03-20 19:45:17 +0100

    chip: irq handling with AON_PROG mitigation

--- chips/cc2650/src/chip.rs
+++ chips/cc2650/src/chip.rs
@@ -1,4 +1,4 @@
-use core::fmt::Write;
+use core::{arch::asm, fmt::Write};
 
 use cortexm3::{nvic, CortexM3, CortexMVariant as _};
 
@@ -33,21 +33,53 @@ impl kernel::platform::chip::Chip for Cc2650 {
     fn service_pending_interrupts(&self) {
         unsafe {
             while let Some(interrupt) = nvic::next_pending_with_mask(MASK_AON_PROG) {
-                // let irq = NvicIrq::from_u32(interrupt)
-                //     .expect("Pending IRQ flag not enumerated in NviqIrq");
-                // match irq {
-                //     NvicIrq::Gpio => gpio::PORT.handle_interrupt(),
-                //     NvicIrq::AonRtc => rtc::RTC.handle_interrupt(),
-                //     NvicIrq::Uart0 => uart::UART0.handle_interrupt(),
-                //     NvicIrq::I2c0 => i2c::I2C0.handle_interrupt(),
-                //     // We need to ignore JTAG events since some debuggers emit these
-                //     NvicIrq::AonProg => (),
-                //     _ => panic!("Unhandled interrupt {:?}", irq),
-                // }
-                // unimplemented!("Interrupt handling");
+                match interrupt {
+                    irq::GPIO => todo!(),
+                    irq::I2C => todo!(),
+                    irq::RF_CORE_PE_1 => todo!(),
+                    irq::AON_RTC => todo!(),
+                    irq::UART0 => todo!(),
+                    irq::UART1 => todo!(),
+                    irq::SSI0 => todo!(),
+                    irq::SSI1 => todo!(),
+                    irq::RF_CORE_PE_2 => todo!(),
+                    irq::RF_CORE_HW => todo!(),
+                    irq::RF_CMD_ACK => todo!(),
+                    irq::I2S => todo!(),
+                    irq::WATCHDOG => todo!(),
+                    irq::GPT0A => todo!(),
+                    irq::GPT0B => todo!(),
+                    irq::GPT1A => todo!(),
+                    irq::GPT1B => todo!(),
+                    irq::GPT2A => todo!(),
+                    irq::GPT2B => todo!(),
+                    irq::GPT3A => todo!(),
+                    irq::GPT3B => todo!(),
+                    irq::CRYPTO => todo!(),
+                    irq::DMA_SD => todo!(),
+                    irq::DMA_ERROR => todo!(),
+                    irq::FLASH => todo!(),
+                    irq::SW_EVENT_0 => todo!(),
+                    irq::AUX_COMBINED => todo!(),
+
+                    // We need to ignore JTAG events since some debuggers emit these
+                    irq::AON_PROG => asm! {
+                        "nop",
+                        "nop",
+                        "nop",
+                    },
+
+                    irq::DYNAMIC_PROG => todo!(),
+                    irq::AUX_COMP_A => todo!(),
+                    irq::AUX_ADC => todo!(),
+                    irq::TRNG => todo!(),
+                    _ => panic!("Unhandled interrupt {}", interrupt),
+                }
                 let n = nvic::Nvic::new(interrupt);
                 n.clear_pending();
-                n.enable();
+                if interrupt != irq::AON_PROG {
+                    n.enable();
+                }
             }
         }
     }
