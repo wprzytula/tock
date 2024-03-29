e2fb455e7efdaba8736bd503933e044594ee5f1f
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2024-03-29 17:37:31 +0100

    chip: handle interrupts in InterruptService

--- chips/cc2650/src/chip.rs
+++ chips/cc2650/src/chip.rs
@@ -1,6 +1,7 @@
 use core::{arch::asm, fmt::Write};
 
 use cortexm3::{nvic, CortexM3, CortexMVariant as _};
+use kernel::platform::chip::InterruptService as _;
 
 use crate::{
     gpt::Gpt,
@@ -62,48 +63,9 @@ impl kernel::platform::chip::Chip for Cc2650<'_> {
     fn service_pending_interrupts(&self) {
         unsafe {
             while let Some(interrupt) = nvic::next_pending_with_mask(MASK_AON_PROG) {
-                match interrupt {
-                    irq::GPIO => todo!(),
-                    irq::I2C => todo!(),
-                    irq::RF_CORE_PE_1 => todo!(),
-                    irq::AON_RTC => todo!(),
-                    irq::UART0 => todo!(),
-                    irq::UART1 => todo!(),
-                    irq::SSI0 => todo!(),
-                    irq::SSI1 => todo!(),
-                    irq::RF_CORE_PE_2 => todo!(),
-                    irq::RF_CORE_HW => todo!(),
-                    irq::RF_CMD_ACK => todo!(),
-                    irq::I2S => todo!(),
-                    irq::WATCHDOG => todo!(),
-                    irq::GPT0A => self.gpt.handle_interrupt(),
-                    irq::GPT0B => unreachable!(),
-                    irq::GPT1A => unreachable!(),
-                    irq::GPT1B => unreachable!(),
-                    irq::GPT2A => unreachable!(),
-                    irq::GPT2B => unreachable!(),
-                    irq::GPT3A => unreachable!(),
-                    irq::GPT3B => unreachable!(),
-                    irq::CRYPTO => todo!(),
-                    irq::DMA_SD => todo!(),
-                    irq::DMA_ERROR => todo!(),
-                    irq::FLASH => todo!(),
-                    irq::SW_EVENT_0 => todo!(),
-                    irq::AUX_COMBINED => todo!(),
+                let supported = self.service_interrupt(interrupt);
+                assert!(supported, "Got unsupported interrupt: {}", interrupt);
 
-                    // We need to ignore JTAG events since some debuggers emit these
-                    irq::AON_PROG => asm! {
-                        "nop",
-                        "nop",
-                        "nop",
-                    },
-
-                    irq::DYNAMIC_PROG => todo!(),
-                    irq::AUX_COMP_A => todo!(),
-                    irq::AUX_ADC => todo!(),
-                    irq::TRNG => todo!(),
-                    _ => panic!("Unhandled interrupt {}", interrupt),
-                }
                 let n = nvic::Nvic::new(interrupt);
                 n.clear_pending();
                 if interrupt != irq::AON_PROG {
@@ -132,5 +94,55 @@ impl kernel::platform::chip::Chip for Cc2650<'_> {
 
     unsafe fn print_state(&self, writer: &mut dyn Write) {
         CortexM3::print_cortexm_state(writer);
+    }
+}
+
+impl kernel::platform::chip::InterruptService for Cc2650<'_> {
+    unsafe fn service_interrupt(&self, interrupt: u32) -> bool {
+        match interrupt {
+            irq::GPIO => todo!(),
+            irq::I2C => todo!(),
+            irq::RF_CORE_PE_1 => todo!(),
+            irq::AON_RTC => todo!(),
+            irq::UART0 => todo!(),
+            irq::UART1 => todo!(),
+            irq::SSI0 => todo!(),
+            irq::SSI1 => todo!(),
+            irq::RF_CORE_PE_2 => todo!(),
+            irq::RF_CORE_HW => todo!(),
+            irq::RF_CMD_ACK => todo!(),
+            irq::I2S => todo!(),
+            irq::WATCHDOG => todo!(),
+            irq::GPT0A => self.gpt.handle_interrupt(),
+            irq::GPT0B => unreachable!(),
+            irq::GPT1A => unreachable!(),
+            irq::GPT1B => unreachable!(),
+            irq::GPT2A => unreachable!(),
+            irq::GPT2B => unreachable!(),
+            irq::GPT3A => unreachable!(),
+            irq::GPT3B => unreachable!(),
+            irq::CRYPTO => todo!(),
+            irq::DMA_SD => todo!(),
+            irq::DMA_ERROR => todo!(),
+            irq::FLASH => todo!(),
+            irq::SW_EVENT_0 => todo!(),
+            irq::AUX_COMBINED => todo!(),
+
+            // We need to ignore JTAG events since some debuggers emit these
+            // These `nop`s are for debug purposes only.
+            irq::AON_PROG => asm! {
+                "nop",
+                "nop",
+                "nop",
+            },
+
+            irq::DYNAMIC_PROG => todo!(),
+            irq::AUX_COMP_A => todo!(),
+            irq::AUX_ADC => todo!(),
+            irq::TRNG => todo!(),
+            _ => return false,
+        }
+
+        true
     }
 }
