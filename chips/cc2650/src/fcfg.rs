f3759774b3e34a72f6ad0e0151870a5e66dea027
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2024-05-13 09:21:02 +0200

    fcfg: read ieee mac address

--- chips/cc2650/src/fcfg.rs
+++ chips/cc2650/src/fcfg.rs
@@ -7,4 +7,8 @@ impl Fcfg {
         Self { fcfg }
     }
 
+    pub fn ieee_mac(&self) -> u64 {
+        ((self.fcfg.mac_15_4_1.read().addr_32_63().bits() as u64) << 32)
+            + self.fcfg.mac_15_4_0.read().addr_0_31().bits() as u64
+    }
 }
